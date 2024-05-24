13d38988dac9db850f89b24cb8285a2dd38367fa
katejoyce07 <166703062+katejoyce07@users.noreply.github.com>
2024-05-24 15:03:16 +0100

    Add files via upload

---
+++ MatplotLearning.py
@@ -0,0 +1,490 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+print(matplotlib.__version__)
+
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+# The plot() function is used to draw points (markers) in a diagram.
+# By default, the plot() function draws a line from point to point.
+# The function takes parameters for specifying points in the diagram.
+# Parameter 1 is an array containing the points on the x-axis.
+# Parameter 2 is an array containing the points on the y-axis.
+# If we need to plot a line from (1, 3) to(8, 10), we have to pass two arrays[1, 8] and [3, 10] to the plot function.
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+# Plotting without a line
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+
+# multiple points
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+
+plt.plot(ypoints)
+plt.show()
+
+
+# markers
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker='o')
+plt.show()
+
+# with star
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker='*')
+plt.show()
+
+# Format Strings
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, 'o:r')
+plt.show()
+
+
+# marker size
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker='o', ms=20)
+plt.show()
+
+# marker colour
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker='o', ms=20, mec='r')
+plt.show()
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker='o', ms=20, mfc='r')
+plt.show()
+
+# linestyle
+
+ypoints = np.array([3, 8, 1, 10])
+
+
+plt.plot(ypoints, linestyle='dotted')
+plt.show()
+
+# Shorter Syntax
+# The line style can be written in a shorter syntax:
+# linestyle can be written as ls.
+# dotted can be written as: .
+# dashed can be written as --.
+
+
+# line colour
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, color='r')
+plt.show()
+
+# line width
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linewidth='20.5')
+plt.show()
+
+
+# multiple lines
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(y1)
+plt.plot(y2)
+
+plt.show()
+
+
+# create axis labels
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+
+# add title
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+
+
+# set font for lables and title
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+font1 = {'family': 'serif', 'color': 'blue', 'size': 20}
+font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}
+
+plt.title("Sports Watch Data", fontdict=font1)
+plt.xlabel("Average Pulse", fontdict=font2)
+plt.ylabel("Calorie Burnage", fontdict=font2)
+
+plt.plot(x, y)
+plt.show()
+
+# position title
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data", loc='left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+plt.show()
+
+
+# adding gridlines
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid()
+
+plt.show()
+
+
+# only for x
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis='x')
+
+plt.show()
+
+
+# draw 2 plots
+# plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y)
+
+# plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y)
+
+plt.show()
+
+
+# using subplot
+# plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 1, 1)
+plt.plot(x, y)
+
+# plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 1, 2)
+plt.plot(x, y)
+
+plt.show()
+
+# draw as many
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 1)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 2)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 3)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 4)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 5)
+plt.plot(x, y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 6)
+plt.plot(x, y)
+
+plt.show()
+
+# add titles to 2
+# plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y)
+plt.title("SALES")
+
+# plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y)
+plt.title("INCOME")
+
+plt.show()
+
+
+# Super Title
+# plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y)
+plt.title("SALES")
+
+# plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y)
+plt.title("INCOME")
+
+plt.suptitle("MY SHOP")
+plt.show()
+
+# Scatter Plot
+x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+
+plt.scatter(x, y)
+plt.show()
+
+# 2 plots on same
+# day one, the age and speed of 13 cars:
+x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+plt.scatter(x, y)
+
+# day two, the age and speed of 15 cars:
+x = np.array([2, 2, 8, 1, 15, 8, 12, 9, 7, 3, 11, 4, 7, 14, 12])
+y = np.array([100, 105, 84, 105, 90, 99, 90, 95, 94, 100, 79, 112, 91, 80, 85])
+plt.scatter(x, y)
+
+plt.show()
+
+# color of individual marker
+x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+colors = np.array(["red", "green", "blue", "yellow", "pink", "black",
+                  "orange", "purple", "beige", "brown", "gray", "cyan", "magenta"])
+
+plt.scatter(x, y, c=colors)
+
+plt.show()
+
+
+# Add colour map
+x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+
+plt.scatter(x, y, c=colors, cmap='viridis')
+
+plt.colorbar()
+
+plt.show()
+
+
+# Size
+
+x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+sizes = np.array([20, 50, 100, 200, 500, 1000, 60, 90, 10, 300, 600, 800, 75])
+
+plt.scatter(x, y, s=sizes)
+
+plt.show()
+
+
+# transparency using alpha
+x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+sizes = np.array([20, 50, 100, 200, 500, 1000, 60, 90, 10, 300, 600, 800, 75])
+
+plt.scatter(x, y, s=sizes, alpha=0.5)
+
+plt.show()
+
+
+# combine colour and alpha
+x = np.random.randint(100, size=(100))
+y = np.random.randint(100, size=(100))
+colors = np.random.randint(100, size=(100))
+sizes = 10 * np.random.randint(100, size=(100))
+
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.5, cmap='nipy_spectral')
+
+plt.colorbar()
+
+plt.show()
+
+
+# Bars
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y)
+plt.show()
+
+
+x = ["APPLES", "BANANAS"]
+y = [400, 350]
+plt.bar(x, y)
+plt.show()
+
+# vertical bars
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y)
+plt.show()
+
+# adjust size
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, width=0.1)
+plt.show()
+
+
+# histograms
+x = np.random.normal(170, 10, 250)
+
+print(x)
+
+x = np.random.normal(170, 10, 250)
+
+plt.hist(x)
+plt.show()
+
+# Pie charts
+y = np.array([35, 25, 25, 15])
+
+plt.pie(y)
+plt.show()
+
+# add labels
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels=mylabels)
+plt.show()
+
+# Start Angle
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels=mylabels, startangle=90)
+plt.show()
+
+
+# explode
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+myexplode = [0.2, 0, 0, 0]
+
+plt.pie(y, labels=mylabels, explode=myexplode)
+plt.show()
+
+# add shadows
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+myexplode = [0.2, 0, 0, 0]
+
+plt.pie(y, labels=mylabels, explode=myexplode, shadow=True)
+plt.show()
+
+# add legends
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels=mylabels)
+plt.legend()
+plt.show()
+
+
+# with header
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels=mylabels)
+plt.legend(title="Four Fruits:")
+plt.show()
